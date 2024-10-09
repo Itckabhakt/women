d1b8b0a60bcb0c25bae745f5e4223c346c635ab2
Itckabhakt <165606700+Itckabhakt@users.noreply.github.com>
2024-10-09 20:25:54 +0530

    Create Shaka.js

---
+++ Shaka.js
@@ -0,0 +1,56 @@
+const channelData = {
+  "Penglish1": {
+    "file": "https://aba5sdmaaaaaaaamo56zosqr4m7j7.otte.live.cf.ww.aiv-cdn.net/pdx-nitro/live/clients/dash/enc/0y0tptbbjk/out/v1/8b1b95c2e6dc496abe14a3ae5c675917/cenc.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "e90836d23b4ec1b5618b2e4f3a470d38",
+        "key": "08f968dd99717b22564fe1089258eb40"
+      }
+    }
+  },
+  "Penglish2": {
+    "file": "https://aba5sdmaaaaaaaamd2pqhgokntneg.otte.live.cf.ww.aiv-cdn.net/pdx-nitro/live/clients/dash/enc/mfvez6cbji/out/v1/108bfdad52584b3dac8da45d0b679e7d/cenc.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "29d389f476fe821be2fa93f48377cf66",
+        "key": "a9edff53a726d123acfed56e68d0299e"
+      }
+    }
+  },
+  "Phindi": {
+    "file": "https://a120aivottepl-a.akamaihd.net/pdx-nitro/live/clients/dash/enc/cikydktuif/out/v1/c0373ca1f8a441c29bb4dbb193c4a39c/cenc.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "913680cbb31c57ae4004ec030b5ec223",
+        "key": "5ea27cc20c4fa3474e67c1a8083e45c9"
+      }
+    }
+  },
+  "Willowcricket": {
+    "file": "https://cors-proxy.cooks.fyi/https://dfwlive-v1-c1p3-sponsored.s.llnwi.net/Content/HLS.cps/Live/channel(WILLHD-3291.dfw.1080)/index.m3u8",
+    "drm": {
+      "clearkey": {
+        "keyId": "59ed8dc9d4a48857ef8e5865919496cc",
+        "key": "0d3680bd8b349deda8b7fdd1da71b7f8"
+      }
+    }
+  },
+  "Astroplus": {
+    "file": "https://linear05-playback.sooka.my/CH2/masterCH2.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "4a267ba09b3a43969364ee77f76dd622",
+        "key": "5b00d4341e90b3022b5e57139ae8ab16"
+      }
+    }
+  },
+  "Skysportscricket": {
+    "file": "https://linear007-gb-dash1-prd-cf.cdn.skycdp.com/016a/Content/DASH_003_720_120/Live/channel(skysportscricket)/manifest_720-120.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "00053ac1e9b1231eed3eb4e3540b3ae1",
+        "key": "0f03d8541826af3d9acbac975d751165"
+      }
+    }
+  },
+};
